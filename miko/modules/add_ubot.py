55c456c956b10096d7bb2dc6a463933f37b4a8ca
ᴊɪʏᴏ <150880343+JIYOXC@users.noreply.github.com>
2024-04-01 22:20:35 +0700

    Update add_ubot.py

--- miko/modules/add_ubot.py
+++ miko/modules/add_ubot.py
@@ -2,31 +2,31 @@ from miko import *
 
 
 
-@PY.CALLBACK("gratis")
+@MIKO.CALLBACK("gratis")
 async def _(client, callback_query):
     await bikin_gratis(client, callback_query)
 
 
-@PY.CALLBACK("st12")
+@MIKO.CALLBACK("st12")
 async def _(client, callback_query):
     await ohaja(client, callback_query)
 
 
-@PY.CALLBACK("bubt")
+@MIKO.CALLBACK("bubt")
 async def _(client, callback_query):
     await bikin_memek(client, callback_query)
 
 
-@PY.CALLBACK("bahan")
+@MIKO.CALLBACK("bahan")
 async def _(client, callback_query):
     await need_api(client, callback_query)
 
 
-@PY.CALLBACK("bayar_dulu")
+@MIKO.CALLBACK("bayar_dulu")
 async def _(client, callback_query):
     await payment_userbot(client, callback_query)
 
 
-@PY.CALLBACK("add_ubot")
+@MIKO.CALLBACK("add_ubot")
 async def _(client, callback_query):
     await bikin_ubot(client, callback_query)
