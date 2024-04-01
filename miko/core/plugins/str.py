fb460e04a46564f599f42d6338f39de0d2e620d8
ᴊɪʏᴏ <150880343+JIYOXC@users.noreply.github.com>
2024-04-01 21:51:20 +0700

    Add files via upload

---
+++ miko/core/plugins/str.py
@@ -0,0 +1,87 @@
+import asyncio
+from datetime import datetime
+from gc import get_objects
+from time import time
+
+from asyncio import sleep
+
+from platform import python_version
+from pyrogram import __version__
+
+from pyrogram.raw.functions import Ping
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+from miko import *
+
+
+
+async def ping_cmd(client, message):
+    start = datetime.now()
+    await client.invoke(Ping(ping_id=0))
+    end = datetime.now()
+    delta_ping = (end - start).microseconds / 1000
+    emot_1 = await get_vars(client.me.id, "EMOJI_PING")
+    emot_2 = await get_vars(client.me.id, "EMOJI_MENTION")
+    emot_3 = await get_vars(client.me.id, "EMOJI_BOT")
+    emot_pong = emot_1 if emot_1 else "5269563867305879894"
+    emot_mention = emot_2 if emot_2 else "5006123161918374529"
+    emot_userbot = emot_3 if emot_3 else "5362049330825927922"
+    if client.me.is_premium:
+        _ping = f"""
+<b><emoji id={emot_pong}>🏓</emoji>PONG !!!:</b> <code>{str(delta_ping).replace('.', ',')} ms</code>
+<b><emoji id={emot_mention}>👑</emoji>USERS !!!:</b> <a href=tg://user?id={client.me.id}>{client.me.first_name} {client.me.last_name or ''}</a>
+<b><emoji id={emot_userbot}>😎</emoji>POWERED BY:</b> <code>ᴍɪᴋᴏ ᴜʙᴏᴛ</code>
+"""
+    else:
+        _ping = f"""
+<b>Menyala!!!</b>\n<code>{delta_ping}ms</code>
+"""
+    await message.reply(_ping)
+
+
+async def start_cmd(client, message):
+    if len(message.command) < 2:
+        buttons = Button.start(message)
+        msg = MSG.START(message)
+        await message.reply(msg, reply_markup=InlineKeyboardMarkup(buttons))
+    else:
+        txt = message.text.split(None, 1)[1]
+        msg_id = txt.split("_", 1)[1]
+        send = await message.reply("<b>ᴛᴜɴɢɢᴜ sᴇʙᴇɴᴛᴀʀ...</b>")
+        if "secretMsg" in txt:
+            try:
+                m = [obj for obj in get_objects() if id(obj) == int(msg_id)][0]
+            except Exception as error:
+                return await send.edit(f"<b>❌ ᴇʀʀᴏʀ:</b> <code>{error}</code>")
+            user_or_me = [m.reply_to_message.from_user.id, m.from_user.id]
+            if message.from_user.id not in user_or_me:
+                return await send.edit(
+                    f"<b>❌ ᴘᴇsᴀɴ ɪɴɪ ʙᴜᴋᴀɴ ᴜɴᴛᴜᴋᴍᴜ <a href=tg://user?id={message.from_user.id}>{message.from_user.first_name} {message.from_user.last_name or ''}</a>"
+                )
+            else:
+                text = await client.send_message(
+                    message.chat.id,
+                    m.text.split(None, 1)[1],
+                    protect_content=True,
+                    reply_to_message_id=message.id,
+                )
+                await send.delete()
+                await asyncio.sleep(120)
+                await message.delete()
+                await text.delete()
+        elif "copyMsg" in txt:
+            try:
+                m = [obj for obj in get_objects() if id(obj) == int(msg_id)][0]
+            except Exception as error:
+                return await send.edit(f"<b>❌ ᴇʀʀᴏʀ:</b> <code>{error}</code>")
+            id_copy = int(m.text.split()[1].split("/")[-1])
+            if "t.me/c/" in m.text.split()[1]:
+                chat = int("-100" + str(m.text.split()[1].split("/")[-2]))
+            else:
+                chat = str(m.text.split()[1].split("/")[-2])
+            try:
+                get = await client.get_messages(chat, id_copy)
+                await get.copy(message.chat.id, reply_to_message_id=message.id)
+                await send.delete()
+            except Exception as error:
+                await send.edit(error)
