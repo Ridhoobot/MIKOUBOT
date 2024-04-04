0ebe3db5c81ef02516818d460d1676937d8a4b0c
ᴊɪʏᴏ <150880343+JIYOXC@users.noreply.github.com>
2024-04-05 02:15:54 +0800

    Update joinvc.py

--- miko/modules/joinvc.py
+++ miko/modules/joinvc.py
@@ -27,7 +27,21 @@ Bantuan Untuk Joinvc
 • Perintah: <code>{0}joinvc</code>
 • Penjelasan: Untuk bergabung voice chat grup.
 """
-
+async def get_group_call(
+    client: Client, message: Message, err_msg: str = ""
+) -> Optional[InputGroupCall]:
+    chat_peer = await client.resolve_peer(message.chat.id)
+    if isinstance(chat_peer, (InputPeerChannel, InputPeerChat)):
+        if isinstance(chat_peer, InputPeerChannel):
+            full_chat = (await client.send(GetFullChannel(channel=chat_peer))).full_chat
+        elif isinstance(chat_peer, InputPeerChat):
+            full_chat = (
+                await client.send(GetFullChat(chat_id=chat_peer.chat_id))
+            ).full_chat
+        if full_chat is not None:
+            return full_chat.call
+    await eor(message, f"**No group call Found** {err_msg}")
+    return False
 
 list_data = []
 
