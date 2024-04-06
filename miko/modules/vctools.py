cb34313d05e99cc2a0389066f5c40a45d0998453
ᴊɪʏᴏ <150880343+JIYOXC@users.noreply.github.com>
2024-04-07 04:04:25 +0800

    Update vctools.py

--- miko/modules/vctools.py
+++ miko/modules/vctools.py
@@ -19,7 +19,7 @@ from pyrogram.raw.types import InputGroupCall, InputPeerChannel, InputPeerChat
 from pyrogram.types import Message
 
 from miko import *
-
+from pytgcalls import GroupCallFactory 
 MODULE = "vctools"
 HELP = f"""
 ✘ Bantuan Untuk Voice Chat
