ffb606325a1ce38aa737d16df3677a68c28ea314
ᴊɪʏᴏ <150880343+JIYOXC@users.noreply.github.com>
2024-04-07 02:55:12 +0800

    Update vctools.py

--- miko/modules/vctools.py
+++ miko/modules/vctools.py
@@ -81,7 +81,6 @@ async def join_os(client, message):
             await client.group_call.set_is_mute(True)
             await ky.delete()
           
-
 @MIKO.UBOT("leavevc")
 async def turun_os(client, message):
     # global turun_dewek
