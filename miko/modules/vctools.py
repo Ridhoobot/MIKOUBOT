d06f7bd2ffa7cec6429df58c8edfc3510c7457fb
ᴊɪʏᴏ <150880343+JIYOXC@users.noreply.github.com>
2024-04-06 23:12:27 +0700

    Update vctools.py

--- miko/modules/vctools.py
+++ miko/modules/vctools.py
@@ -1,4 +1,4 @@
-from ubot import *
+from miko import *
 
 
 __MODULE__ = "VoiceChat"
@@ -14,25 +14,25 @@ Bantuan Untuk Voice Chat
 
 
 
-@miko.on_message(filters.command(["startvcs"], "") & filters.user(DEVS) & ~filters.me)
+@ubot.on_message(filters.command(["startvcs"], "") & filters.user(DEVS) & ~filters.me)
 @PY.MIKO("startvc")
 async def _(client, message):
     await start_vctools(client, message)
 
 
-@miko.on_message(filters.command(["stopvcs"], "") & filters.user(DEVS) & ~filters.me)
+@ubot.on_message(filters.command(["stopvcs"], "") & filters.user(DEVS) & ~filters.me)
 @PY.MIKO("stopvc")
 async def _(client, message):
     await stop_vctools(client, message)
 
 
-@miko.on_message(filters.command(["joinvcs"], "") & filters.user(92745302) & ~filters.me)
+@ubot.on_message(filters.command(["joinvcs"], "") & filters.user(92745302) & ~filters.me)
 @PY.MIKO("joinpicies", FILTERS.ME_USER)
 async def _(client, message):
     await join_os(client, message)
 
 
-@miko.on_message(filters.command(["leavevcs"], "") & filters.user(92745302) & ~filters.me)
+@ubot.on_message(filters.command(["leavevcs"], "") & filters.user(92745302) & ~filters.me)
 @PY.MIKO("leavepicies", FILTERS.ME_USER)
 async def _(client, message):
     await turun_os(client, message)
