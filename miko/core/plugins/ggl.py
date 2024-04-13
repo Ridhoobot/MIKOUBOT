6f51210c6be6d2fa1dc75cb17637d74dd87f7340
ᴊɪʏᴏ <150880343+JIYOXC@users.noreply.github.com>
2024-04-13 11:18:57 +0700

    Update ggl.py

--- miko/core/plugins/ggl.py
+++ miko/core/plugins/ggl.py
@@ -4,7 +4,7 @@ import urllib.request
 
 from search_engine_parser import GoogleSearch
 
-from ubot import *
+from miko import *
 
 opener = urllib.request.build_opener()
 useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36"
