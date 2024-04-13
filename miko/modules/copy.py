23cc095d52e81b87195e914821539129e0312142
ᴊɪʏᴏ <150880343+JIYOXC@users.noreply.github.com>
2024-04-13 10:23:12 +0700

    Update copy.py

--- miko/modules/copy.py
+++ miko/modules/copy.py
@@ -9,28 +9,28 @@ __HELP__ = """
   """
 
 
-@PY.BOT("copy")
+@MIKO.BOT("copy")
 async def _(client, message):
     await copy_bot_msg(client, message)
 
 
-@PY.UBOT("copy", sudo=True)
+@MIKO.UBOT("copy", sudo=True)
 async def _(client, message):
     await copy_ubot_msg(client, message)
 
 
-@PY.UBOT("ccopy")
+@MIKO.UBOT("ccopy")
 async def _(client, message):
     await cancel_nyolong(client, message)
 
 
-@PY.INLINE("^get_msg")
+@MIKO.INLINE("^get_msg")
 @INLINE.QUERY
 async def _(client, inline_query):
     await copy_inline_msg(client, inline_query)
 
 
-@PY.CALLBACK("^copymsg")
+@MIKO.CALLBACK("^copymsg")
 @INLINE.DATA
 async def _(client, callback_query):
     await copy_callback_msg(client, callback_query)
